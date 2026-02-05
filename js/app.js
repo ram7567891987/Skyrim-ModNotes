3b3822712ba33094b4c67be0f79be75e4ff3936d
ram7567891987 <ramsattori999@gmail.com>
2026-02-05 12:36:12 +0500

    Add modal section and style with structure for the Skyrim ModNotes UI

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const addBtn = document.querySelector('[data-modal-button]');
+const leftArrow = document.querySelector('.leftArrow');
+const rightArrow = document.querySelector('.rightArrow');
+
+
+// addBtn.addEventListener('click', () => {
+//     console.log('click!');
+    
+// })
