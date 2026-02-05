a06a1eb29c9c685996ec18533bb499d7ff3ebb04
ram7567891987 <ramsattori999@gmail.com>
2026-02-05 13:01:55 +0500

    Update styles and functionality for modal and card components and adding modal window

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-const addBtn = document.querySelector('[data-modal-button]');
+const addBtn = document.querySelector('[data-modal-button]'); 
+const closeBtn = document.querySelector('[data-modal-close]')
+const modal = document.querySelector('[data-modal]');
 const leftArrow = document.querySelector('.leftArrow');
 const rightArrow = document.querySelector('.rightArrow');
 
+// Showing Modal Window
+addBtn.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+})
 
+// Closing Modal Window
+closeBtn.addEventListener('click', () => {
+    modal.classList.add('hidden')
+})
 // addBtn.addEventListener('click', () => {
 //     console.log('click!');
     
