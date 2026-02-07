be5182aec9c5751c4e06c27bb7289c9dfd97173e
ram7567891987 <ramsattori999@gmail.com>
2026-02-07 16:38:09 +0500

    Enhance modal functionality: add buttons for saving and closing, update button styles, and improve layout

---
+++ js/cards.js
@@ -0,0 +1,17 @@
+/* Finding Elements and Creating  Variable */
+const btnSave = document.querySelector('[data-modal-save]')
+
+const modList = document.querySelector('#modList')
+const modForm = document.querySelector('#modForm')
+
+const nameInput = document.querySelector('#name');
+const linkInput = document.querySelector('#link');
+const authorInput = document.querySelector('#auth');
+const infoTextArea = document.querySelector('#info')
+
+
+
+// modForm.addEventListener('click', () => {
+//     console.log("click!!");
+    
+// })
